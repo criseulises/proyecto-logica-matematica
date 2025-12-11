a37d4d0a1f42ea23b6b0735c8b38b4530a3bde30
Cristian Sanchez <scristianeulises@gmail.com>
2025-12-10 22:45:22 -0400

    fix: deploy

---
+++ api/index.py
@@ -0,0 +1,7 @@
+"""
+Vercel Serverless Function wrapper para SmartPlannerX
+"""
+from app import app
+
+# Exportar la app de Flask para Vercel
+# Vercel buscará automáticamente el objeto "app" en este módulo
