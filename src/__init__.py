123e819f75f093ecfd730f7e7b02e1e472304ca5
Cristian Sanchez <scristianeulises@gmail.com>
2025-12-10 22:55:34 -0400

    fix

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,11 @@
 """
-SmartPlannerX - Módulo Principal
+SmartPlannerX - Modulo Principal
 
 Equipo 3:
-- Cristian E. Sánchez R. (25-0688)
-- Hansel Augusto Pérez (25-0461)
+- Cristian E. Sanchez R. (25-0688)
+- Hansel Augusto Perez (25-0461)
 - Lia De Oleo (25-0673)
-- Juan José Cruz Romero (25-0888)
+- Juan Jose Cruz Romero (25-0888)
 - Samir Gonzalez (25-0808)
 - Alejandro Bruno (25-0947)
 - Daniel Osvaldo Lopez (25-0655)
